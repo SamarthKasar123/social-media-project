cb79383ae933d1a40d874b9d3d695492edbbbcb8
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-01-19 14:10:23 +0530

    Some Minor Database Changes

--- social-media-backend/server.js
+++ social-media-backend/server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://socialmediain.netlify.app/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Configure Cloudinary
