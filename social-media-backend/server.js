bafe29ca97897c96b6a373cbd8bcd7d721948ab5
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-01-19 14:24:43 +0530

    Minor Database Changes

--- social-media-backend/server.js
+++ social-media-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://socialmediain.netlify.app/', 'http://localhost:3000'],
+  origin: ['https://socialmediain.netlify.app', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
